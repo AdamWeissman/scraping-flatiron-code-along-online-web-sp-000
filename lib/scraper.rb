e08a220396d75e509790f3be339e237fb1cba551
Adam Weissman <adam@comedytragedyepic.com>
2019-10-10 09:43:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,9 @@ require_relative './course.rb'
 class Scraper
   
   def self.get_page
-  doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-  doc.css(".headline-26OIBN")
+    doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+    doc.css(".headline-26OIBN")
+  end
     
   
   def print_courses
